4398727ac30b356b71061112f1093599317e66f2
zippysolo <z811043@gmail.com>
2024-07-09 22:38:08 +0300

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card';
+import Sidebar from './Components/Sidebar';
+import Topbar from './Components/Topbar';
+import TotalsBox from './Components/TotalsBox';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+
+      <Sidebar></Sidebar>
+      <main className="main-content">
+
+        <Topbar></Topbar>
+        <div class="bottom-container">
+          <div class="bottom-container__left">
+            <div class="box total-box">
+
+              <TotalsBox totalBoxClass="total-box__left" headerText="Total Income" price="583,530.00" currency="USD" percentClass="percentage-increase" percent="20%" percentText="increase compared
+                  to last year"></TotalsBox>
+              <TotalsBox totalBoxClass="total-box__right" headerText="Total Donations" price="50,530.00" currency="USD" percentClass="percentage-decrease" percent="10%" percentText="decrease compared
+                  to last year"></TotalsBox>
+
+            </div>
+          </div>
+        </div>
+      </main>
+
     </div>
   );
 }
