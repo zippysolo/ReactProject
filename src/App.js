9b3ed83ec1e38553de286dd4f917023ff7b4fc9e
zippysolo <z811043@gmail.com>
2024-07-09 22:48:22 +0300

    fixed container class bug

--- src/App.js
+++ src/App.js
@@ -7,21 +7,18 @@ import TotalsBox from './Components/TotalsBox';
 
 function App() {
   return (
-    <div className="Container">
+    <div className="container">
 
       <Sidebar></Sidebar>
       <main className="main-content">
-
         <Topbar></Topbar>
         <div class="bottom-container">
           <div class="bottom-container__left">
             <div class="box total-box">
-
               <TotalsBox totalBoxClass="total-box__left" headerText="Total Income" price="583,530.00" currency="USD" percentClass="percentage-increase" percent="20%" percentText="increase compared
                   to last year"></TotalsBox>
               <TotalsBox totalBoxClass="total-box__right" headerText="Total Donations" price="50,530.00" currency="USD" percentClass="percentage-decrease" percent="10%" percentText="decrease compared
                   to last year"></TotalsBox>
-
             </div>
           </div>
         </div>
